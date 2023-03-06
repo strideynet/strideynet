4edf5c11a3c75ce26148e3e07afedc4eaedf688a
Noah Stride <noah@noahstride.co.uk>
2023-03-06 12:10:41 +0000

    Modify setup

--- setup.sh
+++ setup.sh
@@ -6,8 +6,4 @@ ln -sf $(pwd)/.zshrc $HOME/.zshrc
 # Git
 ln -sf $(pwd)/.gitconfig $HOME/.gitconfig
 
-# Gnupg
-ln -sf $(pwd)/.gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf 
-ln -sf $(pwd)/.gnupg/gpg.conf $HOME/.gnupg/gpg.conf 
-
 ln -sf $(pwd)/ssh-config $HOME/.ssh/config
